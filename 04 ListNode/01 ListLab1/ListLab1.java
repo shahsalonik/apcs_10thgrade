8387525db36b8feaa6a78742a0fa45c73c824108
Saloni Shah <shahsalonik@gmail.com>
2020-11-20 23:25:25 -0500

    Finished ListLab1
    oh my god that was a pain, but it is finished an commented. due on nov 23rd, but it's alr turned in

--- 04 ListNode/01 ListLab1/ListLab1.java
+++ 04 ListNode/01 ListLab1/ListLab1.java
@@ -1,6 +1,7 @@
-// Name:     
-// Date:
+// Name: Saloni Shah   
+// Date: 11/23/2020 (due date)
 import java.util.*;
+
 public class ListLab1
 {
    public static void main(String[] args)
@@ -21,31 +22,31 @@ public class ListLab1
       
       /**** uncomment the code below for ListLab1 Assignment  ****/
       
-      // ListNode a = copyNode(head);
-      // System.out.println("The head has a value \"" + head.getValue() + "\" at "+ head);
-      // System.out.println("The copy of head has a value of \"" + a.getValue() + "\" at "+ a);
+      ListNode a = copyNode(head);
+      System.out.println("The head has a value \"" + head.getValue() + "\" at "+ head);
+      System.out.println("The copy of head has a value of \"" + a.getValue() + "\" at "+ a);
    // 
-      // System.out.print("Copy the list: ");
-      // ListNode copy = copyList(head);
-      // print(copy);
+      System.out.print("Copy the list: ");
+      ListNode copy = copyList(head);
+      print(copy);
    // 
-      // System.out.print("The rest of the list: ");
-      // ListNode theRest = rest(copy);
-      // print(theRest);
+      System.out.print("The rest of the list: ");
+      ListNode theRest = rest(copy);
+      print(theRest);
    // 
-      // System.out.println("First of the rest = " + first(theRest));
-      // System.out.println("Second of the rest = " + second(theRest));
-      // ListNode p = pointerToLast(theRest);
-      // System.out.println("Pointer to Last = " + p.getValue()+ " at " + p);
-      // ListNode c = copyOfLast(theRest);
-      // System.out.println("Copy of Last =    " + c.getValue()+ " at " + c);
+      System.out.println("First of the rest = " + first(theRest));
+      System.out.println("Second of the rest = " + second(theRest));
+      ListNode p = pointerToLast(theRest);
+      System.out.println("Pointer to Last = " + p.getValue()+ " at " + p);
+      ListNode c = copyOfLast(theRest);
+      System.out.println("Copy of Last =    " + c.getValue()+ " at " + c);
    // 
-      // Scanner in = new Scanner(System.in);
-      // System.out.print("Insert what? ");
-      // String x = in.next();
-      // theRest = insertFirst(theRest, x);
-      // theRest = insertLast(theRest, x);
-      // print(theRest);
+      Scanner in = new Scanner(System.in);
+      System.out.print("Insert what? ");
+      String x = in.next();
+      theRest = insertFirst(theRest, x);
+      theRest = insertLast(theRest, x);
+      print(theRest);
    }
    public static void print(ListNode head)
    {
@@ -61,7 +62,178 @@ public class ListLab1
    }
    
    /* enter your code here */
+   
+   
+   /**
+    * Copies the node specified in the argument
+    * @param arg the node to be copied
+    * @return a new ListNode that is an exact copy of the one passed.
+    */
+   public static ListNode copyNode(ListNode arg) {
+      ListNode next = arg.getNext();
+      Object val = arg.getValue();
       
+      ListNode copyArg = new ListNode(val, next);
+      
+      return copyArg;
+   }
+   
+   /**
+    * Copies the whole linked list
+    * @param arg the head of the list
+    * @return the copied list
+    */
+   public static ListNode copyList(ListNode arg) {
+      
+      ListNode list;
+      
+      if(arg.getNext() == null) {
+         list = new ListNode(arg.getValue(), null);
+      }
+      else {
+         list = new ListNode(arg.getValue(), copyList(arg.getNext()));
+      }
+      return list;
+   }
+   
+   /**
+    * Copies everything in the list except for the head
+    * @param h the list
+    * @return a copied list without the head
+    */
+   public static ListNode rest(ListNode h) {
+      
+      ListNode list;
+      
+      if(h.getNext() == null) {
+         return null;
+      }
+      else {
+         list = new ListNode(h.getNext().getValue(), rest(h.getNext()));
+      }
+      return list;
+      
+   }
+   
+   /**
+    * Returns the value of the head of the list
+    * @param head the head of the linked list
+    * @return the value of the head
+    */
+   public static Object first(ListNode head) {
+      if(head == null) {
+         return null;
+      }
+      else {
+         return head.getValue();
+      }
+   }
+   
+   /**
+    * Returns the value of the second node of the list
+    * @param head the head of the linked list
+    * @return the value of the second node of the list
+    */
+   public static Object second(ListNode head) {
+      if(head.getNext() == null) {
+         return 0;
+      }
+      else {
+         return head.getNext().getValue();
+      }  
+   }
+   
+   /**
+    * Makes the pointer (the head) point to the last node
+    * @param head the head of the list
+    * @return the new head (the last node)
+    */
+   public static ListNode pointerToLast(ListNode head) {
+      
+      ListNode pointer;
+      
+      if(head == null) {
+         return null;
+      }
+      else {
+         while(head.getNext() != null) {
+            head = head.getNext();
+         }
+      }
+      
+      return head;
+      
+   }
+   
+   /**
+    * Returns a copy of the last node
+    * @param head the head of the list
+    * @return a copy of the last node
+    */
+   public static ListNode copyOfLast(ListNode head) {
+      
+      ListNode last = null;
+      
+      if(head == null) {
+         return null;
+      }
+      else if(head.getNext() == null) {
+         last = new ListNode(head.getValue(), null);
+         return last;
+      }
+      else {
+      
+         while(head.getNext() != null) {
+            head = head.getNext();
+         }
+         
+         last = new ListNode(head.getValue(), null);
+         return last;
+      }
+   
+   }
+   
+   /**
+    * Inserts a new value as the head
+    * @param head the head of the list
+    * @param arg the value of the new ListNode
+    * @return the new head
+    */
+   public static ListNode insertFirst(ListNode head, Object arg) {
+         
+      if(head == null) {
+         head = new ListNode(arg, null);
+         return head;
+      }
+      else {
+         head = new ListNode(arg, head);
+         return head;
+      }
+   } 
+   
+   /**
+    * Inserts a ListNode at the end of the list
+    * @param head the head of the list
+    * @param arg the value of the new ListNode
+    * @return the list
+    */
+   public static ListNode insertLast(ListNode head, Object arg) {
+      if(head == null) {
+         return head;
+      }
+      else {
+         ListNode pointer;
+         
+         for(pointer = head; pointer != null; pointer = pointer.getNext()) {
+            if(pointer.getNext() == null) {
+               ListNode newLast = new ListNode(arg, null);
+               pointer.setNext(newLast);
+               break;
+            }
+         }
+         return head;
+      }
+   }
       
 }
 
