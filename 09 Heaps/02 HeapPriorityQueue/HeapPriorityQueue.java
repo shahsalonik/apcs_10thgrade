8bd28829b6a43600049e17aa3d96db1f2605e76d
Saloni Shah <shahsalonik@gmail.com>
2021-03-30 23:47:09 -0400

    glitch again

--- 09 Heaps/02 HeapPriorityQueue/HeapPriorityQueue.java
+++ 09 Heaps/02 HeapPriorityQueue/HeapPriorityQueue.java
@@ -1,5 +1,5 @@
- //Name:   
- //Date:
+ //Name: Saloni Shah 
+ //Date: 04/06/2021 (due date)
  
 import java.util.*;
 
@@ -21,37 +21,73 @@ public class HeapPriorityQueue<E extends Comparable<E>>
    
    public boolean add(E obj)
    {
-     
+      myHeap.add(obj);
+      heapUp(1);
+      return true;
    }
    
    public E remove()
    {
-   
+      heapDown(1, myHeap.size());
+      E removed = myHeap.remove(1);
+      return removed;
    }
    
    public E peek()
    {
-   
+      if(myHeap.size() == 1) {
+         return null;
+      }
+      else {
+         return myHeap.get(1);
+      }
    }
    
    public boolean isEmpty()
    {
-     
+      if(myHeap.size() == 0) {
+         return true;
+      }
+      else {
+         return false;
+      }
    }
    
    private void heapUp(int k)
    {
-         
+      int current = myHeap.size() - 1;
+      while(current > 1) {
+         int parent = (current / 2);
+         if(myHeap.get(parent).compareTo(myHeap.get(current)) > 0) {
+            swap(parent, current);
+            current = parent;
+         }
+         else {
+            break;
+         }
+      }
    }
    
    private void swap(int a, int b)
    {
-     
+      E temp = myHeap.get(a);
+      myHeap.set(a, myHeap.get(b));
+      myHeap.set(b, temp);
    }
    
    private void heapDown(int k, int size)
    {
-   
+      if(k > size || 2 * k >= size) {
+         return;
+      }
+      int maxChild = 2 * k;
+      if(maxChild + 1 <= size && myHeap.get(maxChild).compareTo(myHeap.get(maxChild+1)) > 0) {
+         maxChild++;
+      }
+      if(myHeap.get(k).compareTo(myHeap.get(maxChild)) > 0){
+         swap(k, maxChild);
+         heapDown(maxChild, size);
+      }
    }
    
    public String toString()
